d33db06e05408c619da3f70b3960a6c5c79c54bf
haldanciprian <haldanciprian@gmail.com>
2024-04-08 15:16:03 +0300

    fix

--- terraform_cip/V3-EC2-With_VPC.tf
+++ terraform_cip/V3-EC2-With_VPC.tf
@@ -49,7 +49,7 @@ resource "aws_subnet" "dpp-public-subnet-01" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
   tags = {
     Name = "dpp-public-subnet-01"
   }
@@ -59,7 +59,7 @@ resource "aws_subnet" "dpp-public-subnet-02" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1b"
   tags = {
     Name = "dpp-public-subnet-02"
   }
